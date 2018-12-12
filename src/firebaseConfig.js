a5abb75115c4b77211a619d33bf2791cace30144
Leah Garber <leahgarber@Leahs-Air.home>
2018-12-12 12:04:56 -0500

    initial commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyA1JvcH9qsrJ2QiFIJsNnpa2KTrtGIDKH4",
+  authDomain: "book-challenge-data.firebaseapp.com",
+  databaseURL: "https://book-challenge-data.firebaseio.com",
+  projectId: "book-challenge-data",
+  storageBucket: "book-challenge-data.appspot.com",
+  messagingSenderId: "338095951986"
+};
