6ae421ac8c0179efd1e16301aa0b9a1fcb0f24ee
Leah Garber <leahgarber@Leahs-Air.home>
2018-12-12 12:09:26 -0500

    initial function deploy success

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
+const express = require('express'); 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const oauth = functions.config().oauth;
+const webApp = express()
+
+webApp.get( '/auth', (req, res) => {
+
+  res.send( "my client id is: " + oauth.client_id  )
+})
+
+exports.oauth = functions.https.onRequest( webApp )
